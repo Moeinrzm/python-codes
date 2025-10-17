f3414034827b6fab71d59987d3fe8cb2e6e3a006
Moein <moein.razmi.dev@gmail.com>
2025-10-17 22:13:51 +0330

    Add files via upload

---
+++ Speech-to-Text Conventor by speech_recognition .py
@@ -0,0 +1,21 @@
+import speech_recognition
+
+def speech_to_text():
+    microphone=speech_recognition.Recognizer()
+    with speech_recognition.Microphone() as source:
+        microphone.adjust_for_ambient_noise(source)
+        print("I'm trying to hear you:")
+        audio=microphone.listen(source)
+        try:
+            print("Recognizing:")
+            phrase=microphone.recognize_google(audio,language="en")
+            print(f"you said:{phrase}")
+        except speech_recognition.UnknownValueError:
+            print("Sorry, I could not understand the audio.")
+        except speech_recognition.RequestError:
+            print("Check your internet connection.")            
+            
+if __name__ == "__main__":
+    speech_to_text()           
+
+
