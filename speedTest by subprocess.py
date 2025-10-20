bc8c32cdae915d0d9d0a03e9cc91c10462c588bb
MoeinRzm <moein.razmi.dev@gmail.com>
2025-10-20 15:03:35 +0330

    adding speedtest project

--- speedTest by subprocess.py
+++ speedTest by subprocess.py
@@ -1,6 +1,6 @@
 import subprocess
 try:
-    output = subprocess.check_output("speedtest-cli", shell=True, universal_newlines=True)
+    output = subprocess.check_output("speedtest-cli",shell=True, universal_newlines=True)
     print(output)
 except subprocess.CalledProcessError as e:
     print(f"system error {e}")    
